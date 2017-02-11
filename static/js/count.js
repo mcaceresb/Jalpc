feb7ac85d325c533fc845c3d0d0ac47b6c102562
Mauricio Caceres <mauricio.caceres.bravo@gmail.com>
2017-02-10 22:15:52 -0500

    Updated Jalpc theme for it to serve as personal beta page

--- static/js/count.js
+++ static/js/count.js
@@ -4,13 +4,14 @@ $(document).ready(function() {
     $.ajax({
         type: "get",
         async: false,
-        url: "https://vps.jack003.com:8443/api/v1.0/jalpc/pv_count",
+        url: "",
         dataType: "jsonp",
         jsonp: "callback",
         jsonpCallback: "flightHandler",
         success: function(json) {
             var website_count = json.data;
-            $("#jalpc_site_pv").html('<span class="navy">' + website_count + '</span>&nbsp;<span data-i18n="link.view">views</span>&nbsp;||&nbsp;' + pre_content);
+            // $("#jalpc_site_pv").html('<span class="navy">' + website_count + '</span>&nbsp;<span data-i18n="link.view">views</span>&nbsp;||&nbsp;' + pre_content);
+            $("#jalpc_site_pv").html(pre_content);
         },
         error: function() {
             console.log('fail');
